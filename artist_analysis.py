487a8b39cdd4eed437efe698b69cb6c3453555eb
Amen1Ftirich <amenallahftirich@gmail.com>
2026-07-24 20:33:12 -0500

    removed 0 minuteds lsitened in the df

--- artist_analysis.py
+++ artist_analysis.py
@@ -1,2 +1,5 @@
-from combined_data import alldataframed
+from combined_data import totaldata
 import pandas as pd
+
+time_sort_artist =  totaldata.sort_values(by='ms_played', ascending=False)
+print(time_sort_artist)
--- combined_data.py
+++ combined_data.py
@@ -1,7 +1,32 @@
 import pandas as pd
 import glob
+import pylance as pl
 
 allfiles = glob.glob('data/Streaming_History_Audio_*.json')
 df_allfiles = pd.concat((pd.read_json(f) for f in allfiles), ignore_index=True)
 alldataframed = pd.DataFrame(df_allfiles)
-print(alldataframed.info())
+columns_to_drop = [
+    "ts",
+    "platform",
+    "conn_country",
+    "ip_addr",
+    "spotify_track_uri",
+    "episode_name",
+    "episode_show_name",
+    "spotify_episode_uri",
+    "reason_start",
+    "reason_end",
+    "shuffle",
+    "skipped",
+    "offline",
+    "offline_timestamp",
+    "incognito_mode",
+    "audiobook_title",
+    "audiobook_uri",
+    "audiobook_chapter_uri",
+    "audiobook_chapter_title",
+
+]
+totaldata = df_allfiles.drop(columns= columns_to_drop)
+ms_remove = totaldata[totaldata["ms_played"] = 0]
+print(totaldata)
