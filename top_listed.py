7d224cc29650f73e5303b6b9055d02110a4c65ec
Amen1Ftirich <amenallahftirich@gmail.com>
2026-07-25 21:07:25 -0500

    top 10 with the artist name

--- top_listed.py
+++ top_listed.py
@@ -5,15 +5,15 @@ import pandas as pd
 
 artist_data = totaldata["master_metadata_album_artist_name"].value_counts(ascending= False, sort= True)
 top_10_artists = artist_data.head(10).rename_axis("artist").reset_index(name = "play_count")
-album_data = totaldata["master_metadata_album_album_name"].value_counts(ascending= False, sort= True)
-top_10_albums =album_data.head(10).rename_axis("album").reset_index(name= "play_count")
+album_data = totaldata[["master_metadata_album_album_name", "master_metadata_album_artist_name"]].value_counts(ascending= False, sort= True)
+top_10_albums =(album_data.head(10).reset_index(name = "play_count"))
 top_10_albums.index += 1
 top_10_artists.index += 1
-track_data = totaldata["master_metadata_track_name"].value_counts(ascending=False, sort = True)
-top_10_tracks = track_data.head(10).rename_axis("track").reset_index(name="play counts")
+track_data = totaldata[["master_metadata_track_name", "master_metadata_album_artist_name"]].value_counts(ascending=False, sort = True)
+top_10_tracks = (track_data.head(10).reset_index(name="play counts"))
 
 
 
 print(top_10_tracks)
-#print(top_10_albums)
+print(top_10_albums)
 #print(top_10_artists)
