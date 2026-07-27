22e3b288f15e91a042def4be84640b9fa23b48b5
Amen1Ftirich <amenallahftirich@gmail.com>
2026-07-26 20:27:13 -0500

    finished yearly top artist and track

--- timestampeddata_2026.py
+++ timestampeddata_2026.py
@@ -15,9 +15,12 @@ def get_year_data(dataframe, year):
     return filtered_data
 year_Data = get_year_data(totaldata, selected_year)
 artist_id_year = year_Data['master_metadata_album_artist_name'].value_counts().idxmax()
+album_id_year = year_Data['master_metadata_album_album_name'].value_counts().idxmax()
+track_id_year = year_Data['master_metadata_track_name'].value_counts().idxmax()
 
 
 
 print(f"Your top artist in {selected_year} is {artist_id_year}")
-
+print(f"Your top track in {selected_year} is {track_id_year}")
+print(f"Your top album in {selected_year} is {album_id_year}")
     
