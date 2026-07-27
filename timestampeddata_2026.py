9b81dca7062f4b1d484e5e319182fcf704b88dbb
Amen1Ftirich <amenallahftirich@gmail.com>
2026-07-26 20:23:45 -0500

    Finished the yearly top artist

--- timestampeddata_2026.py
+++ timestampeddata_2026.py
@@ -1,11 +1,23 @@
 import glob as glob
 import pandas as pd
 from combined_data import totaldata
-#year_need = int(input("Which year do you want to get data from?"))
+selected_year = int(input("Which year do you want to get data from?"))
 #converting ts to clear time stamps:
 totaldata['ts'] = pd.to_datetime(totaldata['ts'], utc= True)
-print(totaldata)
 totaldata["year"] = totaldata['ts'].dt.year
 totaldata["month"] = totaldata['ts'].dt.month_name()
 totaldata["day"] = totaldata['ts'].dt.day
-print(totaldata.head(10))
+
+def get_year_data(dataframe, year):
+    filtered_data = dataframe[dataframe['year'] == selected_year ].copy()
+    if filtered_data.empty:
+        return None
+    return filtered_data
+year_Data = get_year_data(totaldata, selected_year)
+artist_id_year = year_Data['master_metadata_album_artist_name'].value_counts().idxmax()
+
+
+
+print(f"Your top artist in {selected_year} is {artist_id_year}")
+
+    
